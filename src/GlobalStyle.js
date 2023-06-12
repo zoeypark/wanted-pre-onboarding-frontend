5a0ddf347ff657a8f68b628931b2291d990ff0cb
Zoe Park <mary990t@gmail.com>
2023-06-13 01:08:47 +0900

    chore: Signin 페이지 마크업

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,36 +1,64 @@
 import { createGlobalStyle } from "styled-components";
  
 const GlobalStyles = createGlobalStyle`
-    // 적용시킬 css 입력
-    a{
-        text-decoration: none;
-        color: inherit;
-    }
-    *{
-        box-sizing: border-box;
-    }
-    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-    a, dl, dt, dd, ol, ul, li, form, label, table{
-        margin: 0;
-        padding: 0;
-        border: 0;
-        font-size: 10px;
-        vertical-align: baseline;
-    }
-    body{
-        line-height: 1;
-        font-family: 'Noto Sans KR', sans-serif;
-        background-color: #F6F9F0;
-        margin-bottom: 100px;
-    }
-    ol, ul{
-        list-style: none;
-    }
-    button {
-        border: 0;
-        background: transparent;
-        cursor: pointer;
-    }
+ * {
+    box-sizing : border-box;
+ }
+ html {
+    font-size : 62.5%;
+ }
+ body {
+    margin : 0;
+    font-size:var(--main-font);
+    font-family: 'Noto Sans KR', sans-serif;
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    background-color:#fafafa;
+ }
+ ul,ol {
+    list-style : none;
+ }
+ a {
+    text-decoration : none;
+ }
+ a, ul,ol, p, h1, h2, h3, h4, h5 {
+    margin : 0;
+    padding : 0;
+    color : var(--font-color);
+ }
+#root {
+   width:100%;
+   min-height: 100vh;
+   max-width:50rem;
+   background-color: var(--white-color);
+}
+input {
+   width:100%;
+   border-radius:5px;
+   border:1px solid var(--border-color);
+   padding: 1.5rem;
+   height: 4.5rem;
+}
+input:focus,textarea:focus {
+   outline:none;
+   border:1px solid var(--point-color);
+}
+label {
+   font-size: 1.4rem;
+   color:var(--sub-font-color);
+   margin-bottom: .5rem;
+   display: block;
+}
+textarea{
+   width:100%;
+   border-radius: 5px;
+   border:1px solid var(--border-color);
+   height: 10rem;
+   padding: 1.5rem;
+   resize: none;
+}
 `;
  
 export default GlobalStyles;
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,65 @@
+import { styled } from "styled-components";
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10rem;
+  gap: 1.5rem;
+  > button {
+    width: 100%;
+    padding: 1rem;
+    border: none;
+    border-radius: 1rem;
+    color: white;
+    background-color: #D9D9D9;
+  }
+  > button:hover {
+      background-color: #838383;
+      cursor: pointer;
+    }
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 2rem;
+  >.signinBtn {
+    border-radius: 1rem;
+    border: none;
+    padding: 1rem;
+    color: white;
+    background-color: black;
+  }
+  >.signinBtn:hover {
+    cursor: pointer;
+  }
+`
+
 const Signin = () => {
   return (
     <>
-    <div>signin</div>
+      <StyledContainer>
+        <StyledForm>
+          <div>
+            <label>email</label>
+            <input data-testid="email-input" placeholder="enter your email"></input>
+          </div>
+          <div>
+            <label>password</label>
+            <input data-testid="password-input" placeholder="enter your password"></input>
+          </div>
+          <button 
+            data-testid="signin-button" 
+            className="signinBtn" 
+            type="submit">Signin</button>
+        </StyledForm>
+        <button>Create an account</button>
+      </StyledContainer>
     </>
   )
 }
