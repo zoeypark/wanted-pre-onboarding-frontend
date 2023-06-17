8d1832893815d9636927983b45113a49d1efd952
Zoe Park <mary990t@gmail.com>
2023-06-17 22:16:39 +0900

    feat: todo list 불러오기 및 todo 추가 기능 구현

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import axiosInstance from "../util/axios";
 
 const StyledContainer = styled.div`
   width: 100%;
@@ -51,6 +52,9 @@ const StyledContainer = styled.div`
 const Todo = () => {
   const navigate = useNavigate();
   
+  const [todos, setTodos] = useState([]);
+  const [newTodo,setNewTodo] = useState('');
+
   useEffect(()=>{
     const accessToken = localStorage.getItem("accessToken");
     if(!accessToken) {
@@ -59,22 +63,74 @@ const Todo = () => {
     }
   }, [navigate]);
 
+  const accessToken = localStorage.getItem("accessToken");
+
+  const headers = {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }
+
+  useEffect(() => {
+    getTodos();
+  },[]);
+
+  const getTodos = async() => {
+    try {
+      const res = await axiosInstance.get(
+        '/todos',
+        headers
+      )
+      console.log(res.data);
+      setTodos(res.data);
+    } catch(e) {
+      console.log(e);
+    };
+  }
+
+  const createTodoBtnClick = () => {
+    createTodo();
+  }
+
+  const createTodo = async() => {
+    try {
+      const res = await axiosInstance.post(
+        '/todos',
+        {
+          todo: newTodo
+        },
+        headers
+      )
+      console.log(res);
+      if(res.status === 201) {
+        getTodos();
+        setNewTodo('');
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  console.log(newTodo);
+
   return (
     <>
       <StyledContainer>
         <div>
-          <input data-testid="new-todo-input" placeholder="Create new todo"></input>
-          <button data-testid="new-todo-add-button">+add</button>
+          <input data-testid="new-todo-input" placeholder="Create new todo" onChange={(e)=>setNewTodo(e.target.value)} value={newTodo}></input>
+          <button data-testid="new-todo-add-button" onClick={createTodoBtnClick}>+add</button>
         </div>
         <ul>
-          <li>
-              <label>
-                <input type="checkbox"/>
-                <span>rmfwkRkwl </span>
-              </label>
-              <button data-testid="modify-button">edit</button>
-              <button data-testid="delete-button">delete</button>
-          </li>
+          {todos.map((todo)=>{
+            return(
+              <li key={todo.id}>
+                <label>
+                  <input type="checkbox" defaultChecked={todo.isCompleted}/>
+                  <span>{todo.todo}</span>
+                </label>
+                <button data-testid="modify-button">edit</button>
+                <button data-testid="delete-button">delete</button>
+              </li>
+            )
+          })}
         </ul>
       </StyledContainer>
     </>
